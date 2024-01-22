6bfc0fe183a38ec0570f846225673ba3724e67c9
ajith9505 <ajith.aj987@gmail.com>
2024-01-22 19:53:54 +0530

    update 2

--- db/db.js
+++ db/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 //Configuring database
 const connectDB = async() => {
-    await mongoose.connect('mongodb://0.0.0.0:27017/mentor-assign-app',{
+    await mongoose.connect(process.env.DB_URL,{
         useNewUrlParser : true,
         useUnifiedTopology : true
     })
